b8054d66c22807d6e24a37e744904fca8726b18c
Tobias Fischer <info@tobiasfischer.info>
2021-12-17 06:48:04 +1000

    Revert "Merge pull request #20 from Tobias-Fischer/patch-1"
    This reverts commit 76ec8ee3ee15d4258206c369d903af7963922681, reversing
    changes made to f2fda6529bd4b101cd2ba893e8eb83b569b0c715.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-mkdir build
-cd build
-
-cmake ${CMAKE_ARGS} .. \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DBUILD_SHARED_LIBS=ON \
-      -DCMAKE_INSTALL_LIBDIR=lib
-
-cmake --build . --config Release -- -j$CPU_COUNT
-cmake --build . --config Release --target install
-
-if [[ "${build_platform}" == "linux-64" ]]; then
-  patchelf --set-soname libyaml-0.so.2 $PREFIX/lib/libyaml.so
+./bootstrap
+./configure --prefix="${PREFIX}"
+make
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+make check
 fi
+make install
